080d6155f8bcb5496e353e690df57564b771752e
Takuya Murakami <tmurakam@tmurakam.org>
2023-02-08 21:22:45 +0900

    chore: output more info

--- output.tf
+++ output.tf
@@ -1,3 +1,13 @@
-output "vm_default_ips" {
+output "vm_ips" {
   value = [ for value in vsphere_virtual_machine.vm : value.default_ip_address ]
 }
+
+output "vm" {
+  value = [ for value in vsphere_virtual_machine.vm : {
+    name = value.name
+    default_ip = value.default_ip_address
+    guest_ips = value.guest_ip_addresses
+    host_system_id = value.host_system_id
+  }]
+}
+
